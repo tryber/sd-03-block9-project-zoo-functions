06b812ca316544f1e22b7fa747a98aee8d750cbd
Lucas Cardoso <lsc.cardosoo@gmail.com>
2020-03-31 16:51:30 -0300

    employeeByName function cc

--- src/zoo.js
+++ src/zoo.js
@@ -38,7 +38,9 @@ function animalsOlderThan(animal, age) {
 function employeeByName(employeeName) {
   if (employeeName === undefined) return {};
   const funcionarioFiltrado = funcionarios.filter((funcionario) => {
-    if (funcionario.firstName === employeeName || funcionario.lastName === employeeName) return true;
+    if (funcionario.firstName === employeeName || funcionario.lastName === employeeName) {
+      return true;
+    } 
     return false;
   });
   return funcionarioFiltrado[0];
