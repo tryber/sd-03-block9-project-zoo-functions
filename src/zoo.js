4808db200b13faee4d84fe44d7c7a6e59cc3fc3c
ilan274 <ilan.herbach@gmail.com>
2020-03-31 16:08:53 -0300

    Initial commit

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,12 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
-}
+const animalsByIds = (ids) => {
+  const animais = data.animals.filter(el => el.id === ids);
+  return animais;
+};
+
+console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
