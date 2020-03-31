bdcb32790de95e96bd40cf320a1893e8daadd5d3
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-31 17:21:28 -0300

    5: allright

--- src/zoo.js
+++ src/zoo.js
@@ -46,6 +46,9 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
+  return data.employees
+    .some(({ managers }) => managers
+      .some(managerId => managerId === id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
