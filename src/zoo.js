6c40c2b6a02b200795056be04c483ae4c81158d1
luis.campos <gucampos845@gmail.com>
2020-03-31 17:04:14 -0300

    Correção codeClimate

--- src/zoo.js
+++ src/zoo.js
@@ -30,8 +30,8 @@ function animalsByIds(...ids) {
 function animalsOlderThan(animal, age) {
   // seu código aqui
   const especies = animals.filter(bicho => (bicho.name === animal));
-  const idade = especies.map(especies => especies.residents);
-  const idades = idade[0].every(idade => idade.age >= age);
+  const idade = especies.map(especie => especie.residents);
+  const idades = idade[0].every(ida => ida.age >= age);
   return idades;
 }
 
