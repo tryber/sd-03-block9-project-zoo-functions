df80e86a78be4562ff6373ed9fdd75637ae6db8b
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-31 16:47:31 -0300

    Adicionando funcao addEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -29,9 +29,29 @@ const createEmployee = (personalInfo, associatedWith) => ({
 
 const isManager = id => data.employees.some(e => e.managers.find(i => i === id));
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+class Employee {
+  constructor(id, firstName, lastName, managers = [], responsibleFor = []) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.managers = managers;
+    this.responsibleFor = responsibleFor;
+  }
 }
+
+const addEmployee = (...args) => data.employees.push(new Employee(...args));
+
+// addEmployee('4141da1c-a6ed-4cf7-90c4-99c657ba4ef3', 'Jane', 'Doe',
+// [
+//   '546fe3d4-2d81-4bb4-83a7-92d5b7048d17',
+//   'a67a36ee-3765-4c74-8e0f-13f881f6588a',
+// ],
+// [
+//   'ee6139bf-b526-4653-9e1e-1ca128d0ad2e',
+//   '210fcd23-aa7b-4975-91b7-0230ebb27b99',
+// ]);
+
+console.log(data.employees);
 
 function animalCount(species) {
   // seu código aqui
