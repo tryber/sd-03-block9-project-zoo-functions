e1100a487a708f2de7217a0070e7cb363f158b13
luis.campos <gucampos845@gmail.com>
2020-03-31 15:54:07 -0300

    Hof

--- src/zoo.js
+++ src/zoo.js
@@ -10,14 +10,13 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
-const assert = require('assert')
 const [...animals] = data.animals;
 
 function animalsByIds(...ids) {
   // seu código aqui
   if (ids.length === 0) {
     return ids
-  };
+  }
 
   const arr = [];
   for (let indice = 0; indice < ids.length; indice += 1) {
