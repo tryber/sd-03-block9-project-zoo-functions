5c6fa1e76c54d63bbffd3abda980b7cefc64f815
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-31 17:20:43 -0300

    animalCount function - CC

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { animals, employees, prices } = data;
+const { animals, employees } = data;
 
 function animalsByIds(...ids) {
   if (ids) {
