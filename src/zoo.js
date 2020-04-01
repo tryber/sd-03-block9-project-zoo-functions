fb3d6a562a568dcecc0761aeb7f06c699ea1fbb6
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-04-01 17:58:28 -0300

    test

--- src/zoo.js
+++ src/zoo.js
@@ -19,28 +19,8 @@ const animalsOlderThan = (animal, age) => data.animals.find(anim => anim.name ==
 const employeeByName = employeeName => (employeeName === undefined ? {} : data.employees
 .find(e => e.firstName === employeeName || e.lastName === employeeName));
 
-const createEmployee = (personalInfo, associatedWith) => {
-  let retornar = Object.assign({}, personalInfo, associatedWith);
-  return retornar
-}
+const createEmployee = (personalInfo, asso) => Object.assign({}, personalInfo, asso);
 
-const personalInfo = {
-  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
-  firstName: 'John',
-  lastName: 'Doe',
-};
-
-const associatedWith = {
-  managers: [
-    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-    '9e7d4524-363c-416a-8759-8aa7e50c0992'
-  ],
-  responsibleFor: [
-    '0938aa23-f153-4937-9f88-4858b24d6bce',
-    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
-  ]
-};
 createEmployee(personalInfo,associatedWith)
 const isManager = id => data.employees.some(ele => ele.managers.find(al => al === id));
 
