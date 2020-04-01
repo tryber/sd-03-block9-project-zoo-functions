f1f50a46ea955779780bd45cc34c7662547b2dfc
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-04-01 13:44:20 -0300

    Att

--- src/zoo.js
+++ src/zoo.js
@@ -15,6 +15,8 @@ const [...animals] = data.animals;
 
 const [...employees] = data.employees;
 
+const {Adult, Child, Senior} = data.prices;
+
 function animalsByIds(...ids) {
   // seu código aqui
   if (ids.length === 0) {
@@ -62,20 +64,62 @@ function isManager(id) {
   return gerents;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+const novoFunc = {id,firstName,lastName,managers,responsibleFor};
+data.employees.push(novoFunc);
 }
 
 function animalCount(species) {
   // seu código aqui
+  if(!species){
+  const quantidade = animals.map(animal => animal.residents.length);
+  const quanAnimal = animals.map(anima => anima.name);
+  const obj = {};
+    for( let i = 0; i < quantidade.length; i+=1){
+   obj [quanAnimal[i]] = quantidade[i];
+  }
+  return obj;
 }
 
-function entryCalculator(entrants) {
+const qtdTotal = animals.filter(animais => animais.name === species);
+const totalqtd = qtdTotal.map(anim => anim.residents.length);
+
+return totalqtd[0];
+}
+
+function entryCalculator(entrants = {}) {
   // seu código aqui
+  if(!entrants || Object.entries(entrants).length===0){
+    return 0;
+  }
+
+  const { Adult: N, Child: A, Senior: B} = entrants;
+  let valorTotal = 0;
+  valorTotal = (N * data.prices.Adult)+(A * data.prices.Child) + (B * data.prices.Senior);
+  return valorTotal;
 }
 
 function animalMap(options) {
   // seu código aqui
+  if(!options){
+    const nEast = animals.filter(local => (local.location === 'NE')
+    ).map(animal => animal.name);
+    const nWest = animals.filter(local => (local.location === 'NW')
+    ).map(animal => animal.name);
+    const sEast = animals.filter(local => (local.location ==='SE')
+    ).map(animal => animal.name);
+    const sWest = animals.filter(local => (local.location === 'SW')
+    ).map(animal => animal.name);
+    const obj = {
+      NE: nEast,
+      NW: nWest,
+      SE: sEast,
+      SW:sWest,
+    }
+    return obj;
+  }
+  
 }
 
 function schedule(dayName) {
