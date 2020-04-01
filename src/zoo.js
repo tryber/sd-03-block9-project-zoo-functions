172d50814f080b256ccac6c920ab1400df6a3a9e
Rodrigo Brezolin <brezolinr@gmail.com>
2020-04-01 18:11:06 -0300

    ex4

--- src/zoo.js
+++ src/zoo.js
@@ -32,9 +32,12 @@ const animalsOlderThan = (animal, age) => data.animals.find(
 const employeeByName = employeeName => data.employees.find(
   element => element.firstName === employeeName || element.lastName === employeeName) || {};
 
-const createEmployee = (personalInfo, associatedWith) => ({
-  ...personalInfo, ...associatedWith
-});
+const createEmployee = (personalInfo, associatedWith) => (
+  {
+    ...personalInfo, 
+    ...associatedWith,
+  }
+);
 
 function isManager(id) {
   // seu código aqui
