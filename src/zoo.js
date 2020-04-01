f83fabcaa71b52fd99f4c2a157a1e1dad14f5df4
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-04-01 02:02:34 -0300

    code

--- src/zoo.js
+++ src/zoo.js
@@ -25,8 +25,10 @@ const createEmployee = (personalInfo, associatedWith) => {
 
 const isManager = id => data.employees.some(ele => ele.managers.find(al => al === id));
 
-const addEmployee = (id=[], firstName=[], lastName=[], managers=[], responsibleFor=[]) => {
-  data.employees.push(Object.assign({}, {id}, {firstName}, {lastName}, {managers}, {responsibleFor}));
+const addEmployee = (id = [], firstName = [], lastName = [], managers = [], responsibleFor = []) => 
+{
+  data.employees.push(Object.assign({}, { id }, { firstName }, { lastName }, { managers },
+     { responsibleFor }));
 };
 
 function animalCount(species) {
