68393b302b104085b61e42400fdcaea7d80015c6
alcantara2509 <alcantara2509@gmail.com>
2020-03-31 21:33:06 -0300

    isManager

--- src/zoo.js
+++ src/zoo.js
@@ -23,9 +23,7 @@ const employeeByName = employeeName =>
 
 const createEmployee = (personalInfo, associatedWith) => ({ ...personalInfo, ...associatedWith });
 
-function isManager(id) {
-  // seu código aqui
-}
+const isManager = id => data.employees.some(m => m.managers.find(mId => mId === id));
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
