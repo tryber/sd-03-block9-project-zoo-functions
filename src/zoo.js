b36815b1af9006e7aff8e8f67571b7d6e65188f9
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-04-01 18:11:40 -0300

    #4 done, rest

--- src/zoo.js
+++ src/zoo.js
@@ -55,7 +55,7 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return {...personalInfo, ...associatedWith};
 }
 
 function isManager(id) {
