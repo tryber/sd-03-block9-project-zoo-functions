e7aa7f1a13015215d570d1d620994ed69e7c6f6f
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-04-01 18:03:36 -0300

    schedule

--- src/zoo.js
+++ src/zoo.js
@@ -56,8 +56,24 @@ function animalMap(options) {
   // seu código aqui
 }
 
-function schedule(dayName) {
-  // seu código aqui
+const  schedule = (dayName) => {
+  const myobj = {};
+
+  if (dayName) {
+    myobj[dayName] = `Open from ${data.hours[dayName].open}am until ${data.hours[dayName].close - 12}pm`;
+
+    if (dayName === 'Monday') {
+      myobj[dayName] = "CLOSED";
+    }
+    return myobj;
+  } 
+
+  Object.keys(data.hours).forEach((e) => {
+    myobj[e] = `Open from ${data.hours[e].open}am until ${data.hours[e].close - 12}pm`;
+    if (e === 'Monday') myobj[e] = 'CLOSED';
+  });
+  return myobj;
+  
 }
 
 function oldestFromFirstSpecies(id) {
