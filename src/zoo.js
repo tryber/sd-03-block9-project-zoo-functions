4f76a4a44e240b90d94c883bd3019ae8f592f9f8
Rhian Castro <orcola@pop-os.localdomain>
2020-04-02 12:41:19 -0300

    Now added delete for real

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(ids) {
-  // seu código aqui@test
+  // seu código aqui@newtest
 }
 
 function animalsOlderThan(animal, age) {
