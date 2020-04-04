b9a236309239af0b5d41f64c1e256459d1559456
felipedoss <felipecaetano25.1996@gmail.com>
2020-04-04 04:08:32 -0300

    animalsByIds codeclimate corrigido

--- src/zoo.js
+++ src/zoo.js
@@ -9,39 +9,75 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
+const data = require("./data");
 
 function animalsByIds(...ids) {
   if (ids.length === 0) return ids;
-  return ids.map(id => data.animals.find(animal => animal.id === id));
+  return ids.map((id) => data.animals.find((animal) => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  if (
+    data.animals.find(
+      (anima) =>
+        anima.name === animal &&
+        anima.residents.find((resident) => resident.age === age)
+    )
+  )
+    return true;
+  return false;
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  return data.employees.find(
+    (employe) =>
+      employe.firstName === employeeName || employe.lastName === employeeName
+  );
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return Object.assign(personalInfo, associatedWith);
 }
 
 function isManager(id) {
-  // seu código aqui
+  if (
+    data.employees.find((employe) =>
+      employe.managers.find((managers) => managers == id)
+    )
+  )
+    return true;
+  return false;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(
+  id,
+  firstName,
+  lastName,
+  managers = [],
+  responsibleFor = []
+) {
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
-  // seu código aqui
+  if (species == undefined)
+    return data.animals.reduce((accumulator, currentValue) => {
+      accumulator[currentValue.name] = currentValue.residents.length;
+      return accumulator;
+    }, {});
+  return data.animals.find((animal) => animal.name == species).residents.length;
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (entrants == undefined || Object.keys(entrants) == 0) return 0;
+  return Object.entries(entrants)
+    .map((entry) => {
+      if (entry[0] == "Adult") return entry[1] * 49.99;
+      if (entry[0] == "Child") return entry[1] * 20.99;
+      if (entry[0] == "Senior") return entry[1] * 24.99;
+    })
+    .reduce((accumulator, currentValue) => accumulator + currentValue);
 }
 
 function animalMap(options) {
