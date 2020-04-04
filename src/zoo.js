28574f6d00e9e51f1cedc27ae2cef6e39f0ae23a
Clayton Pereira <cpereiramt@gmail.com>
2020-04-04 16:25:04 -0400

    Salvando o caso de uso createEmployee 2

--- src/zoo.js
+++ src/zoo.js
@@ -48,7 +48,7 @@ function employeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
-  return {...personalInfo,...associatedWith};
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
