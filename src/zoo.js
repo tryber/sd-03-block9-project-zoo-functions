40f10bc78917ce4c5253966e9a2df1ad9c16ba44
Thiago Tavares <thiagooo15@gmail.com>
2020-04-05 14:03:57 -0300

    aaaaa

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,8 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(...ids) {
-  // seu código aqui
+function animalsByIds(ids) {
+  if (ids.length === 0) return [];
 }
 
 function animalsOlderThan(animal, age) {
