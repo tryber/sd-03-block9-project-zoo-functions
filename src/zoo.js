85fde60b1ed96ce8b0d092acf91d2771bbc9da0c
felipedoss <felipecaetano25.1996@gmail.com>
2020-04-06 01:13:09 -0300

    corrigindo o codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -130,9 +130,10 @@ function increasePrices(percentage) {
 
 function employeeCoverage(...idOrName) {
   let employees;
-  if(idOrName.length === 0)
+  if (idOrName.length === 0) {
     employees = data.employees;
-    else employees = [data.employees.find(employe => (employe.id === idOrName[0]) || (employe.firstName === idOrName[0]) || (employe.lastName === idOrName[0]))];
+  } else employees = [data.employees.find(employe =>
+      (employe.id === idOrName[0]) || (employe.firstName === idOrName[0]) || (employe.lastName === idOrName[0]))];
     return employees.reduce((accumulator, currentValue) => {
       accumulator[`${currentValue.firstName} ${currentValue.lastName}`] = currentValue.responsibleFor.map(anim => data.animals.find(a => a.id === anim).name);
     return accumulator;
