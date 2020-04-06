94789227918a6f8430b61583be0ffd3711326164
rogerfaustino <roger_faustino@hotmail.com>
2020-04-06 12:49:26 -0300

    Adding exercises

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(...ids) { // rest parameter
+function animalsByIds(...ids) { // https://www.geeksforgeeks.org/javascript-rest-operator/
   return data.animals.filter(animal => ids.find(id => id === animal.id));
 }
 
@@ -44,23 +44,62 @@ function animalCount(species) {
   //   if (species) return ` ${species.name} ${acc}`;
   //     return data.animals.every(
   //       animal => animal.find(animal => animal.name === species).residents.lenght);
-  // };
+  //
+if (species) {
+  return data.animals.find(animal => animal.name === species).residents.length;
+}
+const quantityOnly = {};
+data.animals.forEach((animal) => { quantityOnly[animal.name] = animal.residents.length; });
+return quantityOnly;
 }
 
-function entryCalculator(entrants) {
-  // seu código aqui
+function entryCalculator(entering) {
+  return entering && Object.keys(entering).length > 0
+  ? Object.keys(entering).reduce((kind, quantity) => (kind + (data.prices[quantity] * entering[quantity])), 0) : 0
 }
 
 function animalMap(options) {
-  // seu código aqui
+  const animalsByLocation = getLocations();
+    
+  };
+  function getLocations() {
+    return data.animals.map(
+      ({ location }) => location)
+      .reduce((acc, location) => {
+        if (acc[location] === undefined) acc[location] = [];
+        return acc;
+      }, {});
 }
 
-function schedule(dayName) {
+// se existe options el em options existe includeNames
+//
+//adicionar em animalsByLocation: animais categorizados por localizaçõia
+//retornar animalsByLocation
+
+// função que recebe por parametro animalsByLocation el ao excutar adiciona
+// para cada animal adicionar seu animal.name em uma location, animalsByLocation
+
+
   // seu código aqui
+
+function schedule(day) {
+  const schedule = {};
+  if (day) {
+    schedule[day] = readableCalendar(day);
+    return schedule;
+  }
+  Object.keys(data.hours).forEach((el) => { schedule[el] = readableCalendar(el); });
+  return schedule;
 }
+
+const readableCalendar = day => ((day === 'Monday')
+  ? 'CLOSED'
+  : `Open from ${data.hours[day].open}am until ${data.hours[day].close - 12}pm`);
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
+  return Object.values(data.animals
+    .find(el => el.id === data.employees.find(animal => animal.id === id).responsibleFor[0]).residents
+    .sort((first, second) => second.age - first.age)[0]);
 }
 
 function increasePrices(percentage) {
