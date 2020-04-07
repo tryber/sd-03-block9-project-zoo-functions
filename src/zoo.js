bb45346b3620dc1cf34639fddff7422b814c3fbd
Mateus Moreira <ashuraw@hotmail.com>
2020-04-07 17:14:37 -0300

    Mateus-Moreira-zoo-functions V1

--- src/zoo.js
+++ src/zoo.js
@@ -12,19 +12,19 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 const animalsByIds = (...ids) =>
-  data.animals.filter((animal) => ids.find((id) => id === animal.id));
+  data.animals.filter(animal => ids.find(id => id === animal.id));
 
 const animalsOlderThan = (animal, age) =>
   data.animals
-    .find((animals) => animals.name === animal)
-    .residents.every((idade) => idade.age >= age);
+    .find(animals => animals.name === animal)
+    .residents.every(idade => idade.age >= age);
 
 function employeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
   }
   const employee = data.employees.find(
-    (nome) => nome.firstName === employeeName || nome.lastName === employeeName
+    nome => nome.firstName === employeeName || nome.lastName === employeeName
   );
   return employee;
 }
@@ -32,8 +32,8 @@ function employeeByName(employeeName) {
 const createEmployee = (personalInfo, associatedWith) =>
   Object.assign(personalInfo, associatedWith);
 
-const isManager = (id) =>
-  data.employees.some(({ managers }) => managers.find((i) => i === id));
+const isManager = id =>
+  data.employees.some(({ managers }) => managers.find(i => i === id));
 
 const addEmployee = (
   id,
@@ -61,13 +61,11 @@ function entryCalculator(entrants) {
   if (entrants === undefined || Object.keys(entrants).length === 0) {
     return 0;
   }
-  {
     return (
       [entrants, data.prices].reduce((soma, item) => item.Adult * soma, 1) +
       [entrants, data.prices].reduce((soma, item) => item.Child * soma, 1) +
       [entrants, data.prices].reduce((soma, item) => item.Senior * soma, 1)
     );
-  }
 }
 
 function animalMap(options) {
