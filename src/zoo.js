17616bc4f0439cdc142ba0201d39775159f620f2
Mateus Moreira <ashuraw@hotmail.com>
2020-04-07 17:00:15 -0300

    Mateus-Moreira-zoo-functions V1

--- src/zoo.js
+++ src/zoo.js
@@ -11,36 +11,63 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
-}
+const animalsByIds = (...ids) =>
+  data.animals.filter((animal) => ids.find((id) => id === animal.id));
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const animalsOlderThan = (animal, age) =>
+  data.animals
+    .find((animals) => animals.name === animal)
+    .residents.every((idade) => idade.age >= age);
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const employee = data.employees.find(
+    (nome) => nome.firstName === employeeName || nome.lastName === employeeName
+  );
+  return employee;
 }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+const createEmployee = (personalInfo, associatedWith) =>
+  Object.assign(personalInfo, associatedWith);
 
-function isManager(id) {
-  // seu código aqui
-}
+const isManager = (id) =>
+  data.employees.some(({ managers }) => managers.find((i) => i === id));
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+const addEmployee = (
+  id,
+  firstName,
+  lastName,
+  managers = [],
+  responsibleFor = []
+) => {
+  const x = { id, firstName, lastName, managers, responsibleFor };
+  return data.employees.push(x);
+};
 
 function animalCount(species) {
-  // seu código aqui
+  if (species !== undefined) {
+    return data.animals.find((ani) => ani.name === species).residents.length;
+  }
+  const obj = {};
+  data.animals.forEach((impar) => {
+    obj[impar.name] = impar.residents.length;
+  });
+  return obj;
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  {
+    return (
+      [entrants, data.prices].reduce((soma, item) => item.Adult * soma, 1) +
+      [entrants, data.prices].reduce((soma, item) => item.Child * soma, 1) +
+      [entrants, data.prices].reduce((soma, item) => item.Senior * soma, 1)
+    );
+  }
 }
 
 function animalMap(options) {
