e179a3ebe12bde116d17a67a2a1a0e47fe026f36
Hfreitas <hfreitas86@gmail.com>
2020-04-06 23:18:12 -0300

    alterações na função increasePrices para adequação ao code climate

--- src/zoo.js
+++ src/zoo.js
@@ -102,11 +102,11 @@ function oldestFromFirstSpecies(id) {
 
 const increasePrices = (percentage) => {
   // seu código aqui
-  Object.keys(data.prices).map((index) => {
+  return Object.keys(data.prices).map((index) => {
     data.prices[index] =
-       Math.round(data.prices[index] * (percentage / 100 + 1) * 100) / 100;
+        Math.round(data.prices[index] * ((percentage / 100) + 1) * 100) / 100;
   });
-}
+};
 
 function employeeCoverage(idOrName) {
   // seu código aqui
