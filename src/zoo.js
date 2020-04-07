ce2d057e11d7be3c307eb991cbe7380562c0580e
lcdlima <leticia.duarte.lima@gmail.com>
2020-04-07 17:33:42 -0300

    Implementing oldestFromFirstSpecies function

--- src/zoo.js
+++ src/zoo.js
@@ -119,11 +119,22 @@ const schedule = (dayName) => {
   return calendar;
 };
 
-schedule();
-
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const oldestFromFirstSpecies = (id) => {
+  let higherAge = 0;
+  let older = {};
+  const choosen = employees.filter(one => one.id === id)
+  const animalId = (choosen[0].responsibleFor)[0];
+  const animalsList = animals.filter(two => two.id === animalId)
+  const residents = animalsList[0].residents;
+  residents.forEach(individual => {
+    if (individual.age > higherAge) {
+      older = individual;
+      higherAge = individual.age;
+    }
+  })
+  const {name, sex, age} = older;
+  return [name, sex, age];
+};
 
 function increasePrices(percentage) {
   // seu código aqui
