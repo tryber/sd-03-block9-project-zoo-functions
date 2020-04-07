e5d643afdddd412a3139c5f44c387ab927a6dc83
DevT <aguiarthiag@gmail.com>
2020-04-07 19:15:11 -0300

    CC 9

--- src/zoo.js
+++ src/zoo.js
@@ -68,7 +68,7 @@ function hours(day) {
 function schedule(dayName) {
   const scheduleV = {};
   if (dayName) {
-    { scheduleV[dayName] = hours(dayName); }
+    scheduleV[dayName] = hours(dayName);
     return scheduleV;
   }
   Object.keys(data.hours).forEach((e) => { scheduleV[e] = hours(e); });
