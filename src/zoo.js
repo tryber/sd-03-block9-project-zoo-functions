66ce5e47c6d75ec74e08b2e678d8c81bdd9fe853
tamagoshii <estelaap_martins@hotmail.com>
2020-04-07 16:51:10 -0300

    erro code climate

--- src/zoo.js
+++ src/zoo.js
@@ -50,12 +50,12 @@ function entryCalculator(entrants) {
     return 0;
   }
   {
-    const valores = [entrants, data.prices]
-    const soma = valores.reduce((soma,item) => item.Adult * soma,1) +
-    valores.reduce((soma,item) => item.Child * soma,1) +
-    valores.reduce((soma,item) => item.Senior * soma,1)
-    return soma
-  } 
+    const valores = [entrants, data.prices];
+    const soma = valores.reduce((soma, item) => item.Adult * soma, 1) +
+    valores.reduce((soma, item) => item.Child * soma, 1) +
+    valores.reduce((soma, item) => item.Senior * soma, 1)
+    return soma;
+  }
 }
 
 function animalMap(options) {
