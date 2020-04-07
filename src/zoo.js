3d8486d0970150964f5ce09c4b17f89af7efe52c
DevT <aguiarthiag@gmail.com>
2020-04-07 18:20:42 -0300

    Segunda parte -map

--- src/zoo.js
+++ src/zoo.js
@@ -49,28 +49,62 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if(entrants && Object.keys(entrants).length > 0){
+    return Object.keys(entrants).reduce((sum, entrant) => (sum +(data.prices[entrant] * entrants[entrant])), 0)
+  }
+  return 0;
 }
 
 function animalMap(options) {
   // seu código aqui
 }
 
+const hours = (day) => (day === 'Monday')? 'CLOSED'
+: `Open from ${data.hours[day].open}am until ${data.hours[day].close - 12}pm`
+
 function schedule(dayName) {
-  // seu código aqui
+  const schedule = {}
+  if (dayName){
+    schedule = {dayName: hours(dayName)}
+    return schedule;
+  }
+  Object.keys(data.hours).forEach(e => { schedule[e] = hours(e) })
+  return schedule;
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
+  const species = data.animals.find(e =>
+  e.id === data.employees.find(e => e.id === id).responsibleFor[0])
+  species.residents.sort((a, b) => b.age - a.age);
+  return [species.residents[0].name, species.residents[0].sex, species.residents[0].age];
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
-}
+Object.keys(data.prices).forEach(e => {
+data.prices[e] = Math.round(data.prices[e] * (1 + (percentage / 100)) * 100) / 100});
+};
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+
+const summary = (e) => {
+  const obj = {};
+  obj[`${e.firstName} ${e.lastName}`] = e
+  .responsibleFor.map(element => data.animals.find(animal => animal.id === element).name);
+  return obj;
+};
+
+function employeeCoverage (idOrName) {
+  const coverage = {};
+  if (idOrName) {
+    const findEmployee = data.employees.find(e => e.id === idOrName
+    || e.firstName === idOrName || e.lastName === idOrName);
+    return summary(findEmployee);
+  }
+  data.employees.forEach((employee) => {
+    Object.assign(coverage, summary(employee));
+  });
+  return coverage;
+};
+
 
 module.exports = {
   entryCalculator,
