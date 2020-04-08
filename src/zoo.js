a1c1b1270352a51ac8886dac39330e3572631aa3
mirellaalves <mirella.alves@gmail.com>
2020-04-08 09:11:58 -0300

    Solving function createEmployee.

--- src/zoo.js
+++ src/zoo.js
@@ -23,7 +23,6 @@ function animalsByIds(ids) {
 // Ao passar o nome de uma espécie e uma idade, testa se todos os animais
 // desta espécie possuem a idade mínima especificada
 function animalsOlderThan(animal, age) {
-  // seu código aqui
   return data.animals.find(anima => anima.name === animal)
                       .residents.every(anima => anima.age >= age);
 }
@@ -32,8 +31,12 @@ function employeeByName(employeeName) {
   // seu código aqui
 }
 
+// 4- Implemente a função createEmployee:
+// Cria um novo colaborador a partir de objetos contendo informações pessoais,
+// gerentes e animais gerenciados
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+    return {...personalInfo, ...associatedWith};
 }
 
 function isManager(id) {
