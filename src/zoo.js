2498b7b823ae7067bc1020aedfad8c353fa26403
Lucas França <luc.rangel90@gmail.com>
2020-04-08 13:20:54 -0300

    Correçoes do cc e outras

--- src/zoo.js
+++ src/zoo.js
@@ -9,19 +9,16 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require("./data");
+const data = require('./data');
 
-const animalsByIds = (...ids) =>
-  data.animals.filter((animal) => ids.find((id) => id === animal.id));
+const animalsByIds = (...ids) => data.animals.filter(animal => ids.find(id => id === animal.id));
 
 const animalsOlderThan = (animal, age) =>
-  data.animals
-    .find((a) => a.name === animal)
-    .residents.every((a) => a.age > age);
+  data.animals.find(a => a.name === animal).residents.every(a => a.age > age);
 
-const employeeByName = (employeeName) =>
+const employeeByName = employeeName =>
   data.employees.find(
-    (e) => e.firstName === employeeName || e.lastName === employeeName
+    e => e.firstName === employeeName || e.lastName === employeeName,
   ) || {};
 
 const createEmployee = (personalInfo, associatedWith) => ({
@@ -29,48 +26,29 @@ const createEmployee = (personalInfo, associatedWith) => ({
   ...associatedWith,
 });
 
-const isManager = (id) =>
-  data.employees.some((e) => e.managers.find((i) => i === id));
+const isManager = id => data.employees.some(e => e.managers.find(i => i === id));
 
-function addEmployee(
-  id,
-  firstName,
-  lastName,
-  managers = [],
-  responsibleFor = []
-) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 const animalCount = (species) => {
   if (species) {
-    return data.animals.find((el) => el.name === species).residents.length;
+    return data.animals.find(el => el.name === species).residents.length;
   }
-};
+}
 
 const entryCalculator = (entrants) => {
   if (entrants && Object.keys(entrants).length > 0) {
-    return Object.keys(entrants).reduce(
-      (acc, el) => acc + data.prices[el] * entrants[el],
-      0
-    );
+    return Object.keys(entrants).reduce((acc, el) => (acc + (data.prices[el] * entrants[el])), 0);
   }
+
   return 0;
 };
 
-const animalMap = (options = {}) => {
-  const { includeNames, sex, sorted } = options;
-  const obj = {};
-  data.animals.forEach((animal) => {
-    obj[animal.location] = data.animals
-      .filter((el) => el.location === animal.location)
-      .map((el) => {
-        if (!includeNames) return el.name;
-        return residName(el.name, sorted, sex);
-      });
-  });
-  return obj;
-};
+function animalMap(options) {
+  // seu código aqui
+}
 
 function schedule(dayName) {
   // seu código aqui
