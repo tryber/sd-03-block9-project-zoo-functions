37d9693269dd5bf9bc36615d876c6cf475b23c32
Gabriel Oliveira Ferreira <gabriel1335555@gmail.com>
2020-04-08 03:53:22 -0300

    animalsById-spread

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
   return data.animals.filter(animal => ids.find(id => id === animal.id));
 }
