b6b539f115c4da4ddae5eae80406c773d0bd09f8
saturnian99 <saturnian.dsg@gmail.com>
2020-04-14 15:12:12 -0300

    Implemented 'animalsByIds' function

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,12 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+// Caso receba nenhum parâmetro, necessário retornar um array vazio;
+// Ao receber como parâmetro um único id, retorna os animais com este id;
+// Ao receber mais de um id, retorna os animais que têm um desses ids.
+function animalsByIds(...ids) {  
+  if(!ids.length) return [];
+  return data.animals.filter(({ id }) => ids.includes(id));
 }
 
 function animalsOlderThan(animal, age) {
